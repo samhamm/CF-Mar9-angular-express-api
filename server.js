fb7cbdaf577f0fedf88c68804cb0fa4b4d5fc22a
samhamm <samhamm@gmail.com>
2015-04-29 21:06:01 -0700

    cleanup

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 'use strict';
 var express = require('express');
-var http = require('http');
 var mongoose = require('mongoose');
-var caplogsRoutes = require('./routes/captains-logs-routes');
+var caplogsRoutes = require('./routes/caplogs-routes');
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/capslogsApp-development');
 
@@ -15,6 +14,6 @@ caplogsRoutes(router);
 
 app.use('/api/v1', router);
 
-http.createServer(app).listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('server listening on port ' + (process.env.PORT || 3000));
 });
